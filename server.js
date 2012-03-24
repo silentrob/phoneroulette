40eb2a5e3d03e5a3ab647e8905ca14b82d24fa18
Rob Ellis <kazoomer@gmail.com>
2012-03-24 15:01:29 -0700

    Twilio Bi-direction working

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var fs = require('fs');
+var qs = require('querystring');
 
 var express = require("express");
-var SessionStore = require('connect-redis')(express);
+var sessionStore = require('connect-redis')(express);
+var request = require('request');
 var port = process.env.PORT || 4000;
 var log = fs.createWriteStream(__dirname + '/log/development.log', {'flags': 'a'});
+
+var config = fs.readFileSync(__dirname + '/config/development.json', 'utf8');
+config = JSON.parse(config);
 
 var API_ENDPOINT = "https://api.twilio.com/2010-04-01";
 
@@ -11,7 +16,7 @@ var app = express.createServer(
   // express.logger(),
   express.bodyParser(),
   express.cookieParser(),
-  express.session({ store: new SessionStore, secret: 'keyboard cat' })  
+  express.session({ store: new sessionStore, secret: 'keyboard cat' })  
 );
 
 app.configure(function(){
@@ -38,10 +43,47 @@ app.post('/twilio', function(req, res){
   var body = params.Body;
   var number = params.From;
   
-  // geme.someMethod(number,body);
+  // game.inbound({number:number,body:body}, function(){
+  // });
   
   res.send('\n', 204);
 });
+
+
+// Twilio Send Message Call
+var sendMessage = function(options, callback) {
+  if (!options.body || !options.phone) {
+    throw new Error("Missing Number and/or Message")
+  }
+  
+  var options = {
+    method:'POST',
+    url : 'https://'+ config.sid + ':' + config.atoken +'@api.twilio.com/2010-04-01/Accounts/' + config.sid + '/SMS/Messages.json',
+    headers : {'content-type':'application/x-www-form-urlencoded'},
+    body : qs.stringify({
+      'Body': options.body,
+      'From': config.fromNumber,
+      'To': options.phone
+    })
+  }
+  
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 201) {
+      callback(201, body)
+    } 
+    if (error) {
+      logger("[ERROR]" + error);
+      callback(response.statusCode, error)
+    }
+  })
+}
+
+app.get('/send',function(req,res){
+  sendMessage({body:'test',phone:'12345'}, function(status){
+    console.log("GO")
+    res.send("DONE");
+  })
+})
 
 app.listen(port);
 console.log("Server Running on Port",  port);
